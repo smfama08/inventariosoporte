11eb0ae53e8c8ba225f99177081012665a6a4585
smfama08 <smfama08@gmail.com>
2026-07-19 18:04:06 -0600

    Correcciones

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,15 @@
+declare const process: {
+  env: {
+    readonly NG_APP_SUPABASE_URL: string;
+    readonly NG_APP_SUPABASE_KEY: string;
+    readonly NG_APP_PRODUCTION?: string;
+  };
+};
+
 export const environment = {
-  production: true,
-  supabaseUrl: 'https://skxrptnqwaxazojpozku.supabase.co',
-  supabaseKey: 'sb_publishable_knOopT7BiSLyLdvbsD9nxw_sKy5OULS',
+  production: process.env.NG_APP_PRODUCTION
+    ? process.env.NG_APP_PRODUCTION === 'true'
+    : true,
+  supabaseUrl: process.env.NG_APP_SUPABASE_URL ?? '',
+  supabaseKey: process.env.NG_APP_SUPABASE_KEY ?? '',
 };
